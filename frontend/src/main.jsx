4e75f93902005abcf8bfccc3e22d103d225635f5
Gaurang Jalgaonkar <gaurangjalgaonkar2004@gmail.com>
2026-04-08 19:54:49 +0530

    fix: use env variable for server URL

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,9 @@ import App from "./App.jsx";
 import { AppProvider } from "./context/AppContext.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 
-export const server = "http://localhost:5000";
+
+export const server = import.meta.env.VITE_SERVER_URL;
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>

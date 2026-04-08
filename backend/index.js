c8ec1266e936fd4d88ed9500842e0989df7ed2f8
Gaurang Jalgaonkar <gaurangjalgaonkar2004@gmail.com>
2026-04-08 21:19:45 +0530

    Final fix cors config

--- backend/index.js
+++ backend/index.js
@@ -62,7 +62,11 @@ try {
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://ai-learning-platform-one-bay.vercel.app",
+  "https://ai-learning-platform-1b7nupplw-gaurang1512s-projects.vercel.app",
+];
 
 app.use(
   cors({
